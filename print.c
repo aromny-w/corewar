8cb929c573428f76260761cd391324a74c96ac2b
Amory lorch Romny weaver <aromny-w@il-k5.21-school.ru>
2020-02-05 18:06:08 +0300

    corewar

--- print.c
+++ print.c
@@ -6,7 +6,7 @@
 /*   By: aromny-w <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/04 16:48:33 by aromny-w          #+#    #+#             */
-/*   Updated: 2020/02/05 17:56:11 by aromny-w         ###   ########.fr       */
+/*   Updated: 2020/02/05 18:03:02 by aromny-w         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	print_status(t_asm *info)
 	i = -1;
 	ft_printf("Name: %s\n", info->header.prog_name);
 	ft_printf("Comment: %s\n", info->header.comment);
-	ft_printf("%28d%24d%24d%24d\n", 1, 2, 3, 4);
+	ft_printf("%2c%8s %17d%24d%24d%24d\n",'#', "Label", 1, 2, 3, 4);
 	while (++i < info->n)
-		ft_printf("%3d: %-6s %-4s [%-21s] [%-21s] [%-21s] [%-21s]\n", i, info->ops[i].label, g_op_tab[info->ops[i].opcode - 1].name, info->ops[i].value[0], info->ops[i].value[1], info->ops[i].value[2], info->ops[i].value[3]);
+		ft_printf("%3d: %-7s %-4s [%-21s] [%-21s] [%-21s] [%-21s]\n", i, info->ops[i].label, g_op_tab[info->ops[i].opcode - 1].name, info->ops[i].value[0], info->ops[i].value[1], info->ops[i].value[2], info->ops[i].value[3]);
 }
