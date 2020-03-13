12752e729e1b7da3db69db275e05ca3246e2778e
Amory lorch Romny weaver <aromny-w@il-k6.21-school.ru>
2020-03-13 17:37:25 +0300

    corewar

--- includes/asm.h
+++ includes/asm.h
@@ -6,7 +6,7 @@
 /*   By: aromny-w <aromny-w@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/22 17:03:06 by aromny-w          #+#    #+#             */
-/*   Updated: 2020/02/23 21:59:52 by aromny-w         ###   ########.fr       */
+/*   Updated: 2020/03/13 17:30:15 by aromny-w         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,56 +20,6 @@
 # include "get_next_line.h"
 # include "ft_dprintf.h"
 
-typedef union	u_value
-{
-	int				nbr;
-	unsigned char	byte[4];
-}				t_value;
-
-typedef struct	s_cmd
-{
-	char			*label;
-	char			*name;
-	unsigned char	opcode;
-	unsigned char	opsize;
-	unsigned char	type[MAX_ARGS_NUMBER];
-	unsigned char	typecode;
-	char			*argbuf[MAX_ARGS_NUMBER];
-	t_value			arg[MAX_ARGS_NUMBER];
-	unsigned char	argsize[MAX_ARGS_NUMBER];
-	unsigned short	pos;
-	struct s_cmd	*next;
-}				t_cmd;
-
-typedef struct	s_asm
-{
-	char			*path;
-	char			*filename;
-	bool			flag;
-	int				fd;
-	char			*line;
-	size_t			index;
-	size_t			lines;
-	t_header		header;
-	bool			name_mark;
-	bool			comment_mark;
-	t_cmd			*op;
-}				t_asm;
-
-void			assembler(char **input);
-void			init_struct(t_asm *info, char **input);
-void			destroy_struct(t_asm *info);
-void			read_file(t_asm *info);
-void			parse_name(t_asm *info);
-void			parse_comment(t_asm *info);
-void			parse_operation(t_asm *info);
-void			parse_opcode(t_asm *info);
-void			parse_arguments(t_asm *info);
-void			parse_argcode(t_asm *info);
-void			set_size(t_asm *info);
-void			write_file(t_asm *info);
-void			print_stdout(t_asm *info);
-void			skip_space(t_asm *info);
-void			terminate(t_asm *info, int status);
+void	assembler(char **input);
 
 #endif
--- srcs/asm/asm.c
+++ srcs/asm/asm.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   asm.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aromny-w <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: aromny-w <aromny-w@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/22 16:57:41 by aromny-w          #+#    #+#             */
-/*   Updated: 2020/02/12 17:49:43 by aromny-w         ###   ########.fr       */
+/*   Updated: 2020/03/13 17:34:40 by aromny-w         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,13 @@
 
 void	assembler(char **input)
 {
-	t_asm	info;
-
-	init_struct(&info, input);
-	read_file(&info);
-	if (!info.flag)
-		write_file(&info);
-	else
-		print_stdout(&info);
-	destroy_struct(&info);
+	(void)input;
 }
 
 int		main(int argc, char **argv)
 {
 	if (argc > 1)
-		assembler(argv);
+		assembler(++argv);
 	else
 		ft_printf("Usage: ./asm [-a] <sourcefile.s>\n");
 	exit(EXIT_SUCCESS);
