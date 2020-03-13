ce58ef5060fda45089de83abf182ab56d34d4e93
Amory lorch Romny weaver <aromny-w@il-k6.21-school.ru>
2020-03-13 17:59:31 +0300

    corewar

--- includes/corewar.h
+++ includes/corewar.h
@@ -6,7 +6,7 @@
 /*   By: aromny-w <aromny-w@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/21 16:03:19 by aromny-w          #+#    #+#             */
-/*   Updated: 2020/02/21 16:34:14 by aromny-w         ###   ########.fr       */
+/*   Updated: 2020/03/13 17:58:36 by aromny-w         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,12 @@
 # define COREWAR_H
 
 # include <fcntl.h>
+# include <stdio.h>
 # include "op.h"
 # include "libft.h"
+# include "get_next_line.h"
 # include "ft_dprintf.h"
 
-void	corewar(char *input);
+void	corewar(char **input);
 
 #endif
--- srcs/corewar/corewar.c
+++ srcs/corewar/corewar.c
@@ -6,13 +6,13 @@
 /*   By: aromny-w <aromny-w@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/22 21:06:12 by aromny-w          #+#    #+#             */
-/*   Updated: 2020/02/21 16:38:50 by aromny-w         ###   ########.fr       */
+/*   Updated: 2020/03/13 17:58:52 by aromny-w         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "corewar.h"
 
-void	corewar(char *input)
+void	corewar(char **input)
 {
 	(void)input;
 }
@@ -20,7 +20,7 @@ void	corewar(char *input)
 int		main(int argc, char **argv)
 {
 	if (argc > 1)
-		corewar(argv[1]);
+		corewar(++argv);
 	else
 		ft_printf("Usage: ./corewar <champion1.cor> <...>\n");
 	return (0);
