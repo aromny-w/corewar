7de59b79e0c7b755c81728301530d30476033812
Gayane <gayane@Gayanes-MBP.Dlink>
2020-07-19 22:24:18 +0300

    corrections to arg byte

--- srcs/operations/live.c
+++ srcs/operations/live.c
@@ -6,7 +6,7 @@
 /*   By: bgilwood <bgilwood@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/17 21:15:12 by bgilwood          #+#    #+#             */
-/*   Updated: 2020/07/16 22:31:17 by bgilwood         ###   ########.fr       */
+/*   Updated: 2020/07/19 20:47:49 by bgilwood         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,12 @@ void	op_live(t_carriage *carriage, t_game_params *params, int arg_code)
 	int	player;
 
 	player = get_argument(params->arena, carriage,
-		(arg_code >> 6) & 3, 0);
+		DIR_CODE, 0);
 	if (player > params->players_num || player < 1)
 		return ;
 	params->last_alive = player;
 	params->live_count_in_period++;
 	live_carriage(carriage, params->cycles_since_start);
 	announce_player_alive(params->players, player);
+	(void)arg_code;
 }
--- srcs/operations/zjmp.c
+++ srcs/operations/zjmp.c
@@ -6,7 +6,7 @@
 /*   By: bgilwood <bgilwood@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/15 22:49:52 by bgilwood          #+#    #+#             */
-/*   Updated: 2020/07/16 22:32:48 by bgilwood         ###   ########.fr       */
+/*   Updated: 2020/07/19 21:08:45 by bgilwood         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,11 @@ void	op_zjmp(t_carriage *carriage, t_game_params *params, int arg_code)
 {
 	int arg;
 
-	arg = get_argument(params->arena, carriage, (arg_code >> 6) & 3, 0);
+	arg = get_argument(params->arena, carriage, DIR_CODE, 0);
 	if (carriage->carry)
 	{
 		move_carriage(carriage, arg);
 		carriage->bytes_next_op = 0;
 	}
+	(void)arg_code;
 }
