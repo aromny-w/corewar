07636d8deb1a00aba4b723fcace73ad884ae76c4
Amory Romny <aromny-w@il-j5.msk.21-school.ru>
2020-08-04 18:11:50 +0300

    live fix (or not?)

--- srcs/corewar/operations/live.c
+++ srcs/corewar/operations/live.c
@@ -16,13 +16,12 @@ void	op_live(t_carriage *carriage, t_game_params *params, int arg_code)
 {
 	int	player;
 
-	player = get_argument(params->arena, carriage,
-		DIR_CODE, 0);
+	player = -get_argument(params->arena, carriage, DIR_CODE, 0);
+	live_carriage(carriage, params->cycles_since_start);
 	if (player > params->players_num || player < 1)
 		return ;
 	params->last_alive = player;
 	params->live_count_in_period++;
-	live_carriage(carriage, params->cycles_since_start);
 	announce_player_alive(params->players, player);
 	(void)arg_code;
 }
