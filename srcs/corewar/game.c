1f949fb053dfa86672935bffbc4208c6b4efc482
Amory Romny <aromny-w@il-j5.msk.21-school.ru>
2020-08-05 17:54:49 +0300

    live + ctd fixes

--- srcs/corewar/game.c
+++ srcs/corewar/game.c
@@ -23,19 +23,21 @@ void	check_carriages(t_car_list_elem **carriages, t_game_params *params)
 	while (elem)
 	{
 		next_elem = elem->next;
-		if (params->cycles_since_start - elem->content->last_live >
+		if (params->cycles_since_start - elem->content->last_live >=
 					params->cycles_to_die)
 			delete_carriage_elem(carriages, elem->content->id);
 		elem = next_elem;
 	}
-	if (!*carriages)
+	if (!*carriages || params->cycles_to_die < 0)
 		game_over(params, *carriages);
 	if (params->live_count_in_period > NBR_LIVE
-			|| params->checks_count > MAX_CHECKS)
+			|| params->checks_count == MAX_CHECKS)
 	{
 		params->cycles_to_die -= CYCLE_DELTA;
+		params->live_count_in_period = 0;
 		params->checks_count = 0;
 	}
+	params->checks_count++;
 	params->live_count_in_period = 0;
 	params->cycles_since_last_check = 0;
 }
@@ -44,8 +46,6 @@ void	play_cycle(t_game_params *params)
 {
 	t_car_list_elem	*list_elem;
 
-	if (params->cycles_since_last_check > params->cycles_to_die)
-		check_carriages(&(params->carriages_list), params);
 	list_elem = params->carriages_list;
 	while (list_elem)
 	{
@@ -55,6 +55,7 @@ void	play_cycle(t_game_params *params)
 			list_elem->content->num_cycles_before_op--;
 		if (!list_elem->content->num_cycles_before_op)
 		{
+
 			exec_op(list_elem->content, params);
 			move_carriage(list_elem->content,
 								list_elem->content->bytes_next_op);
@@ -65,8 +66,10 @@ void	play_cycle(t_game_params *params)
 
 void	play_game(t_game_params *params)
 {
-	while (1)
+	while (true)
 	{
+		if (params->cycles_since_last_check >= params->cycles_to_die)
+			check_carriages(&(params->carriages_list), params);
 		if (params->cycles_since_start == params->dump_idx
 									&& params->dump_flag_on)
 			dump_mem_and_exit(params, params->carriages_list);
--- srcs/corewar/operations/live.c
+++ srcs/corewar/operations/live.c
@@ -18,10 +18,10 @@ void	op_live(t_carriage *carriage, t_game_params *params, int arg_code)
 
 	player = -get_argument(params->arena, carriage, DIR_CODE, 0);
 	live_carriage(carriage, params->cycles_since_start);
+	params->live_count_in_period++;
 	if (player > params->players_num || player < 1)
 		return ;
 	params->last_alive = player;
-	params->live_count_in_period++;
 	announce_player_alive(params->players, player);
 	(void)arg_code;
 }
