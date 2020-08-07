8f80b58d80f7f79e365fd24e1a3315259f38b3f6
Amory Romny <aromny-w@il-k5.msk.21-school.ru>
2020-08-07 17:36:50 +0300

    fixed live check

---
+++ debug.sh
@@ -0,0 +1,3 @@
+gcc -Wall -Wextra -Werror srcs/debug.c -o srcs/debug
+./srcs/debug
+rm srcs/debug
--- srcs/debug.c
+++ srcs/debug.c
@@ -12,30 +12,16 @@
 
 #include <fcntl.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 
 # define PROG_NAME_LENGTH (128)
 # define COMMENT_LENGTH (2048)
 # define COREWAR_EXEC_MAGIC 0xea83f3
 
-typedef struct	s_op
-{
-	int	opcode;
-	int	acb;
-	int	arg[4];
-}				t_op;
+char	*buff = "02 90 00 00 00 31 02 04 54 03 02 03 06 64 03 00 00 08 00 04 09 FF F3 04 54 03 03 03 03 70 01 00 00 03 70 01 00 00 02 D0 00 64 10 01 00 00 00 00 09 FF FB";
 
-static void	write_op(int fd, t_op op)
-{
-	write(fd, &op.opcode, sizeof(char));
-	write(fd, &op.acb, sizeof(char));
-	write(fd, &op.arg[0], sizeof(char));
-	write(fd, &op.arg[1], sizeof(char));
-	write(fd, &op.arg[2], sizeof(char));
-	write(fd, &op.arg[3], sizeof(char));
-}
-
-static void	write_number(int fd, int n, size_t size)
+void	write_number(int fd, int n, size_t size)
 {
 	char	byte[4];
 
@@ -53,7 +39,13 @@ static void	write_number(int fd, int n, size_t size)
 		write(fd, &byte[0], sizeof(char));
 }
 
-static void	write_header(int fd, char *name, int size, char *comment)
+void	write_code(int fd, size_t size)
+{
+	while (size--)
+		write_number(fd, strtol(buff, &buff, 16), sizeof(char));
+}
+
+void	write_header(int fd, char *name, int size, char *comment)
 {
 	write_number(fd, COREWAR_EXEC_MAGIC, sizeof(int));
 	write(fd, name, PROG_NAME_LENGTH);
@@ -63,30 +55,27 @@ static void	write_header(int fd, char *name, int size, char *comment)
 	write_number(fd, 0, sizeof(int));;
 }
 
-// 11 11 11 11 = 0xff
-// 11 11 11 10 = 0xfe
-// 11 11 11 01 = 0xfd
-// 11 11 11 00 = 0xfc
-// 11 11 10 00 = 0xf8
-// 11 11 01 00 = 0xf4
-// 11 11 00 00 = 0xf0
-// 11 10 00 00 = 0xe0
-// 11 01 00 00 = 0xd0
-// 11 00 00 00 = 0xc0
-// 10 00 00 00 = 0x80
-// 01 00 00 00 = 0x40
-// 00 00 00 00 = 0x00
+size_t	count_bytes(char *str)
+{
+	size_t	count;
 
+	count = 0;
+	while (*str)
+		if (*str++ != ' ')
+			count++;
+	return (count / 2);
+}
 
-int			main(void)
+int		main(void)
 {
 	int		fd;
-	t_op	op = {0x02, 0xff, 0xff, 0xff, 0xff, 0xff};
+	size_t	size;
 
 	fd = open("debug.cor", O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (write(fd, NULL, 0) == -1)
 		perror(NULL);
-	write_header(fd, "", 6, "");
-	write_op(fd, op);
+	size = count_bytes(buff);
+	write_header(fd, "", size, "");
+	write_code(fd, size);
 	close(fd);
 }
